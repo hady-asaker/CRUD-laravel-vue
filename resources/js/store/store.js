362e06ef264cd7f35d5d643cc39ec7626a6c9342
hady-asaker <hadyasaker8@gmail.com>
2023-11-14 09:20:04 +0200

    set Vuex store

---
+++ resources/js/store/store.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import getters from './getters';
+import mutations from './mutations';
+import * as actions from './actions';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+
+    modules: {
+
+    },
+    state: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+
+});
