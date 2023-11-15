798fce675b22e48865320605ed15965ecf42d6d5
hady-asaker <hadyasaker8@gmail.com>
2023-11-16 01:38:49 +0200

    make students table and fix some warnings

--- resources/js/components/student/StudentsTableComponents.vue
+++ resources/js/components/student/StudentsTableComponents.vue
@@ -4,42 +4,76 @@
         <el-button type="success" @click="goAddStudent" size="large" style="margin-left: 28%">Add Student</el-button>
     </el-row>
 
-        <el-table :data="students" style="width: 100%" max-height="250" >
+        <el-table :data="filteredStudents" style="width: 100%" max-height="500" >
+
             <el-table-column prop="name" label="Name" width="120" />
             <el-table-column prop="section" label="Section" width="120" />
             <el-table-column prop="class" label="Class" width="120" />
             <el-table-column prop="email" label="Email" width="400" />
-            <el-table-column fixed="right" label="Edit" width="120">
-                <template #default="scope">
-                    <el-button type="primary" size="medium">Edit</el-button>
+            <el-table-column align="right">
+                <template #header>
+                    <el-input v-model="search" size="default" placeholder="Type to search" />
                 </template>
-            </el-table-column>
-            <el-table-column fixed="right" label="Delete" width="120">
                 <template #default="scope">
-                    <el-button type="danger" size="medium">Remove</el-button>
+                    <el-button type="primary" size="default" @click="editStudent(scope.row)">Edit</el-button>
+                    <el-button type="danger" size="default" @click="removeStudent(scope.row)">Remove</el-button>
                 </template>
             </el-table-column>
         </el-table>
+    <div>
+        <hr class="my-4" />
+        <el-pagination
+            :total="students.length"
+            :page-size="pageSize"
+            layout="prev, pager, next"
+            @current-change="handleCurrentChange"
+        />
+    </div>
+
 </template>
 
 <script>
     export default {
         props: {
-            students: {}
+            students:  '',
         },
         data(){
             return {
-
+                search: '',
+                currentPage: 1,
+                pageSize: 7, // Set your desired page size here
             };
         },
         methods: {
           goAddStudent(){
               window.location.href = "/add-student";
-          }
+          },
+            handleCurrentChange(val) {
+                this.currentPage = val;
+            },
+            editStudent(student){
+              console.log(student);
+            },
+            removeStudent(student){
+                console.log(student);
+            },
         },
         mounted() {
             console.log(this.students);
-        }
+        },
+        computed: {
+            filteredStudents() {
+                const start = (this.currentPage - 1) * this.pageSize;
+                const end = start + this.pageSize;
+                return this.students.filter(
+                    (student) =>
+                        student.name.toLowerCase().includes(this.search.toLowerCase()) ||
+                        student.section.toLowerCase().includes(this.search.toLowerCase()) ||
+                        student.class.toLowerCase().includes(this.search.toLowerCase()) ||
+                        student.email.toLowerCase().includes(this.search.toLowerCase())
+                ).slice(start, end);
+            },
+        },
     };
 
 </script>
