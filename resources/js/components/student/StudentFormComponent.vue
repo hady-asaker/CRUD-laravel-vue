2fe2fe2e01bd9d321228d54604da448b6e849aad
hady-asaker <hadyasaker8@gmail.com>
2023-11-15 02:00:19 +0200

    ElementPlus Form To Add Student

--- resources/js/components/student/StudentFormComponent.vue
+++ resources/js/components/student/StudentFormComponent.vue
@@ -1,71 +1,41 @@
 <template>
-    <h1>This Is Vue component "Form" to add Student</h1>
-    <div>
-        <form>
-            <div class="form-row">
-                <div class="col-md-4 mb-3">
-                    <label for="validationServer01">First name</label>
-                    <input type="text" class="form-control is-valid" id="validationServer01" placeholder="First name" value="Mark" required>
-                    <div class="valid-feedback">
-                        Looks good!
-                    </div>
+    <h1 style="text-align: center">Add Student</h1>
+    <div class="full-height">
+        <el-card class="box-card">
+            <template #header>
+                <div slot="header" class="card-header">
+                    <el-page-header @back="goBack" :content="`Category - ${scope}`">
+                    </el-page-header>
                 </div>
-                <div class="col-md-4 mb-3">
-                    <label for="validationServer02">Last name</label>
-                    <input type="text" class="form-control is-valid" id="validationServer02" placeholder="Last name" value="Otto" required>
-                    <div class="valid-feedback">
-                        Looks good!
-                    </div>
-                </div>
-                <div class="col-md-4 mb-3">
-                    <label for="validationServerUsername">Username</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroupPrepend3">@</span>
-                        </div>
-                        <input type="text" class="form-control is-invalid" id="validationServerUsername" placeholder="Username" aria-describedby="inputGroupPrepend3" required>
-                        <div class="invalid-feedback">
-                            Please choose a username.
-                        </div>
-                    </div>
-                </div>
+            </template>
+
+            <div class="form-container">
+                <el-form :model="model" :rules="rules" ref="studentForm" class="full-form">
+
+                    <el-form-item label="Name" required prop="name" >
+                        <el-input v-model="model.name" aria-placeholder="Student Name" size="large"></el-input>
+                    </el-form-item>
+
+                    <el-form-item label="Class" required prop="class">
+                        <el-input v-model="model.class" aria-placeholder="Student Class" size="large"></el-input>
+                    </el-form-item>
+
+                    <el-form-item label="Section" required prop="section">
+                        <el-input v-model="model.section" aria-placeholder="Student Section" size="large"></el-input>
+                    </el-form-item>
+
+                    <el-form-item label="Email" required prop="email">
+                        <el-input v-model="model.email" aria-placeholder="Student Email" size="large"></el-input>
+                    </el-form-item>
+
+                    <el-row>
+                        <el-form-item>
+                            <el-button type="success" @click="saveForm('studentForm')" size="large">Save Student</el-button>
+                        </el-form-item>
+                    </el-row>
+                </el-form>
             </div>
-            <div class="form-row">
-                <div class="col-md-6 mb-3">
-                    <label for="validationServer03">City</label>
-                    <input type="text" class="form-control is-invalid" id="validationServer03" placeholder="City" required>
-                    <div class="invalid-feedback">
-                        Please provide a valid city.
-                    </div>
-                </div>
-                <div class="col-md-3 mb-3">
-                    <label for="validationServer04">State</label>
-                    <input type="text" class="form-control is-invalid" id="validationServer04" placeholder="State" required>
-                    <div class="invalid-feedback">
-                        Please provide a valid state.
-                    </div>
-                </div>
-                <div class="col-md-3 mb-3">
-                    <label for="validationServer05">Zip</label>
-                    <input type="text" class="form-control is-invalid" id="validationServer05" placeholder="Zip" required>
-                    <div class="invalid-feedback">
-                        Please provide a valid zip.
-                    </div>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="form-check">
-                    <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
-                    <label class="form-check-label" for="invalidCheck3">
-                        Agree to terms and conditions
-                    </label>
-                    <div class="invalid-feedback">
-                        You must agree before submitting.
-                    </div>
-                </div>
-            </div>
-            <button class="btn btn-primary" type="submit">Submit form</button>
-        </form>
+        </el-card>
     </div>
 </template>
 
@@ -87,8 +57,7 @@ export default {
         };
     },
     methods: {
-        savaForm(){
-
+        saveForm(formName){
         },
         goBack(){
             window.location.href = "/students";
@@ -96,10 +65,60 @@ export default {
     },
     mounted() {
         console.log("Vue Mounted");
+        console.log(this.$store);
     }
 }
 </script>
 
 <style scoped>
+
+.full-height {
+    height: 70vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.form-container {
+    //height: 100%;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.full-form {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+}
+
+.text {
+    font-size: 14px;
+}
+
+.item {
+    margin-bottom: 18px;
+}
+/* Increase the size of inputs and buttons */
+.el-input, .el-button {
+    height: 50px;
+    font-size: 18px;
+}
+
+.box-card {
+    width: 100%;
+    margin: 0 auto;
+}
+
+.el-form-item {
+    margin-bottom: 20px;
+}
 
 </style>
