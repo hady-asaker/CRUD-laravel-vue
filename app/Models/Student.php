feb3d0e23cacce4501404cacf48335433d6c9ce5
hady-asaker <hadyasaker8@gmail.com>
2023-11-15 09:06:14 +0200

    edit property

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -10,7 +10,7 @@ class Student extends Model
     use HasFactory;
 
     protected $table = 'students';
-    protected $fillabe = ['name', 'class', 'section', 'email', 'status'];
+    protected $fillable = ['name', 'class', 'section', 'email', 'status'];
     protected $hidden = [];
     public $timestamps = true;
 }
